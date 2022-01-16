0956c489e84afe7c184341e37cd7ad51fa982db8
Dimasavi74 <Dimasavi74comp@gmail.com>
2022-01-17 00:08:49 +0500

    Список изменений: 1. Камера(управление на WASD) 2. Сетка(вызов на клавишу c) 3. Анимация движения в разные стороны(черный цвет потом переделать через альфа-канал или сами картинки) 4. Несколько новых функций в классах 5. Выбор пресонажа по щелчку на него 6. Волновой алгоритм передвижения из точки в точку

---
+++ WaveCell.py
@@ -0,0 +1,40 @@
+def main():
+    lab = [[0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 1, 0, 0, 0, 0],
+           [0, 0, 1, 1, 1, 0, 0],
+           [0, 0, 1, 1, 1, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0]]
+    for yi in range(len(lab)):
+        for xi in range(len(lab[0])):
+            if lab[yi][xi] == 1:
+                lab[yi][xi] = -1
+    rdl = list(map(int, input().split()))
+    x1 = rdl[0] - 1
+    y1 = rdl[1] - 1
+    rdl = list(map(int, input().split()))
+    x2 = rdl[0]
+    y2 = rdl[1]
+    finalout = voln(x1, y1, 1, len(lab), len(lab[0]), lab)
+    for el in finalout:
+        print(el)
+
+
+def voln(x, y, cur, n, m, lab):
+    lab[x][y] = cur
+    if y + 1 < m:
+        if lab[x][y + 1] == 0 or (lab[x][y + 1] != -1 and lab[x][y + 1] > cur):
+            voln(x, y + 1, cur + 1, n, m, lab)
+    if x + 1 < n:
+        if lab[x + 1][y] == 0 or (lab[x + 1][y] != -1 and lab[x + 1][y] > cur):
+            voln(x + 1, y, cur + 1, n, m, lab)
+    if x - 1 >= 0:
+        if lab[x - 1][y] == 0 or (lab[x - 1][y] != -1 and lab[x - 1][y] > cur):
+            voln(x - 1, y, cur + 1, n, m, lab)
+    if y - 1 >= 0:
+        if lab[x][y - 1] == 0 or (lab[x][y - 1] != -1 and lab[x][y - 1] > cur):
+            voln(x, y - 1, cur + 1, n, m, lab)
+    return lab
+
+
+main()
