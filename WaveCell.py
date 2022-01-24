63b96332362fffb323f65a6870f27d947092cf9b
Lexapy <a8F34Xd6B5>
2022-01-24 22:48:18 +0500

    ficks

--- WaveCell.py
+++ WaveCell.py
@@ -8,14 +8,10 @@ def main():
     for yi in range(len(lab)):
         for xi in range(len(lab[0])):
             if lab[yi][xi] == 1:
-                lab[yi][xi] = -1
-    rdl = list(map(int, input().split()))
-    x1 = rdl[0] - 1
-    y1 = rdl[1] - 1
-    rdl = list(map(int, input().split()))
-    x2 = rdl[0]
-    y2 = rdl[1]
-    finalout = voln(x1, y1, 1, len(lab), len(lab[0]), lab)
+                lab[yi][xi] = ''
+    x1, y1 = list(map(int, input().split()))
+    #x2, y2 = list(map(int, input().split()))
+    finalout = voln(x1 - 1, y1 - 1, 1, len(lab), len(lab[0]), lab)
     for el in finalout:
         print(el)
 
@@ -23,16 +19,16 @@ def main():
 def voln(x, y, cur, n, m, lab):
     lab[x][y] = cur
     if y + 1 < m:
-        if lab[x][y + 1] == 0 or (lab[x][y + 1] != -1 and lab[x][y + 1] > cur):
+        if lab[x][y + 1] == 0 or (lab[x][y + 1] != '' and lab[x][y + 1] > cur):
             voln(x, y + 1, cur + 1, n, m, lab)
     if x + 1 < n:
-        if lab[x + 1][y] == 0 or (lab[x + 1][y] != -1 and lab[x + 1][y] > cur):
+        if lab[x + 1][y] == 0 or (lab[x + 1][y] != '' and lab[x + 1][y] > cur):
             voln(x + 1, y, cur + 1, n, m, lab)
     if x - 1 >= 0:
-        if lab[x - 1][y] == 0 or (lab[x - 1][y] != -1 and lab[x - 1][y] > cur):
+        if lab[x - 1][y] == 0 or (lab[x - 1][y] != '' and lab[x - 1][y] > cur):
             voln(x - 1, y, cur + 1, n, m, lab)
     if y - 1 >= 0:
-        if lab[x][y - 1] == 0 or (lab[x][y - 1] != -1 and lab[x][y - 1] > cur):
+        if lab[x][y - 1] == 0 or (lab[x][y - 1] != '' and lab[x][y - 1] > cur):
             voln(x, y - 1, cur + 1, n, m, lab)
     return lab
 
